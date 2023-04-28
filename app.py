5a9e18481506d97cf99e8f66262a920c6dc0d7cc
Cassiano de Souza <cassianodess@hotmail.com>
2023-04-28 12:42:54 -0300

    feat: remembering previos conversation

--- app.py
+++ app.py
@@ -1,45 +1,47 @@
 from os import getenv
-from flask import Flask, make_response, request
-import openai
+from flask import Flask, make_response, request, jsonify
 from flask_cors import CORS
+from models.openai import OpenAI
+from flask_caching import Cache
 
 app = Flask(__name__)
 CORS(app)
 
+app.config["CACHE_TYPE"] = "simple"
+cache = Cache(app)
+
+openai_api = OpenAI(
+    organization=getenv("ORGANIZATION"),
+    api_key=getenv("API_KEY"),
+    model="text-davinci-003"
+)
+
+
 @app.before_request
 def middleware():
-    
-    authorization = request.headers.get("Authorization").split(" ")[1] if request.headers.get("Authorization") else None
+    if request.method != "OPTIONS":
+        authorization = request.headers.get("Authorization")
+        if not authorization or authorization.split(" ")[1] != getenv("SECRET"):
+            return make_response({"message": "Unauthorized"}), 401
 
-    if request.method != "OPTIONS" and not authorization == getenv("SECRET"):
-        return make_response({"message": "Unauthorized"}), 401
 
 @app.route("/api/ask-me", methods=["POST"])
 def ask():
+    question = request.json.get("question")
+    if not question or len(question) == 0:
+        return make_response(jsonify({
+            "status": 400,
+            "message": "Question must not be empty"
+        })), 400
 
-    question = request.json["question"] if request.json["question"] else None
-    openai.organization = getenv("ORGANIZATION") if getenv("ORGANIZATION") else None
-    openai.api_key = getenv("API_KEY") if getenv("API_KEY") else None
+    response = cache.get(question)
 
-    if question == None or len(question) == 0:
-        return make_response({
-        "status": 400,
-        "message": "question must be not empty"
-    }), 400
-        
-    
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=question,
-        temperature=0,
-        max_tokens=2000
-    )
+    if not response:
+        response = openai_api.create_response(question)
+        cache.set(question, response, timeout=60*5)
 
-    return make_response({
+    return make_response(jsonify({
         "status": 200,
         "question": question,
-        "response": response.choices[0].text
-    }), 200
-    
-   
-
+        "response": response
+    })), 200
---
+++ models/openai.py
@@ -0,0 +1,28 @@
+import openai
+
+
+class OpenAI:
+    def __init__(self, organization, api_key, model):
+        self.organization = organization
+        self.api_key = api_key
+        self.model = model
+        self.chat_history = []
+
+    def create_response(self, question):
+        prompt = "Conversation history:\n" + "\n".join(self.chat_history)
+        prompt += f"\nUser: {question}"
+        openai.api_key = self.api_key
+        openai.organization = self.organization
+        response = openai.Completion.create(
+            engine=self.model,
+            prompt=prompt,
+            temperature=0.7,
+            max_tokens=1024,
+            n=1,
+            stop=None,
+            frequency_penalty=0,
+            presence_penalty=0
+        )
+        answer = response.choices[0].text.strip()
+        self.chat_history.append(f"User: {question}\nAI: {answer}")
+        return answer
